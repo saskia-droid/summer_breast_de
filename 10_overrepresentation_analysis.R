cf0f51558ccadd7d5d091e08a2865de8aacb408e
Saskia <saskia.perretgentil@gmail.com>
2021-09-21 15:07:14 +0200

    add overrepresntation analysis

---
+++ 10_overrepresentation_analysis.R
@@ -0,0 +1,43 @@
+
+# overrepresentation analysis
+# saskia perret-gentil 
+
+# run previous script
+#source("06_differential_expression_analysis.R")
+
+# install and load libraries
+BiocManager::install("clusterProfiler")
+BiocManager::install("org.Hs.eg.db")
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(enrichplot)
+
+genes <- row.names(sig)
+universe <- row.names(res)
+
+egoMF <- clusterProfiler::enrichGO( gene          = genes,
+                                    universe      = universe,
+                                    OrgDb         = org.Hs.eg.db,
+                                    ont           = "MF", # molecular function
+                                    pAdjustMethod = "BH",
+                                    pvalueCutoff  = 0.01,
+                                    qvalueCutoff  = 0.05,
+                                    readable      = TRUE,
+                                    keyType       = 'ENSEMBL')
+#head(egoMF)
+dotplot(egoMF, showCategory=20)
+
+# visualize enriched GO terms as a directed acyclic graph: 
+#goplot(egoMF)
+
+egoBP <- clusterProfiler::enrichGO( gene          = genes,
+                                    universe      = universe,
+                                    OrgDb         = org.Hs.eg.db,
+                                    ont           = "BP", # biological process
+                                    pAdjustMethod = "BH",
+                                    pvalueCutoff  = 0.01,
+                                    qvalueCutoff  = 0.05,
+                                    readable      = TRUE,
+                                    keyType       = 'ENSEMBL')
+#head(egoBP)
+dotplot(egoBP, showCategory=20)
